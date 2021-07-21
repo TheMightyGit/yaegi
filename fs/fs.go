7bd987b417c41ff377cc087920432c06188cd54e
Johnny Marshall <the.mighty.git@gmail.com>
2021-07-21 16:37:45 +0100

    update comments

--- fs/fs.go
+++ fs/fs.go
@@ -9,16 +9,17 @@ import (
 	"os"
 )
 
-// We use a type alias to make it easier for the pre-go1.16
+// FS We use a type alias to make it easier for the pre-go1.16
 // code to fullfil this local type.
 type FS = actualFs.FS
 
-// realFS complies with the fs.FS interface.
+// RealFS complies with the fs.FS interface.
 // We use this rather than os.DirFS as DirFS has no concept of
 // what the current working directory is, whereas if we're a simple
 // passthru to os.Open then working dir is automagically taken care of.
 type RealFS struct{}
 
+// Open complies with the fs.FS interface
 func (dir RealFS) Open(name string) (actualFs.File, error) {
 	f, err := os.Open(name)
 	if err != nil {
@@ -28,7 +29,10 @@ func (dir RealFS) Open(name string) (actualFs.File, error) {
 }
 
 var (
-	ReadDir  = actualFs.ReadDir
-	Stat     = actualFs.Stat
+	// ReadDir is an alias to the real implementation. Once the need for backwards compat goes away, so can this.
+	ReadDir = actualFs.ReadDir
+	// Stat is an alias to the real implementation. Once the need for backwards compat goes away, so can this.
+	Stat = actualFs.Stat
+	// ReadFile is an alias to the real implementation. Once the need for backwards compat goes away, so can this.
 	ReadFile = actualFs.ReadFile
 )
