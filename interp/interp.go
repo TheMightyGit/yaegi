3792b12a7cef5f30976a754e7947ad031053f827
Johnny Marshall <the.mighty.git@gmail.com>
2021-07-21 14:17:48 +0100

    fix typo

--- interp/interp.go
+++ interp/interp.go
@@ -245,7 +245,7 @@ func (n *node) Walk(in func(n *node) bool, out func(n *node)) {
 // realFS complies with the fs.FS interface.
 // We use this rather than os.DirFS as DirFS has no concept of
 // what the current working directory is, whereas if we're a simple
-// passthru to os.Open then working dir it automagically taken care of.
+// passthru to os.Open then working dir is automagically taken care of.
 type realFS struct{}
 
 func (dir realFS) Open(name string) (fs.File, error) {
